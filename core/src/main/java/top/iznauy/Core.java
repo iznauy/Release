d382cacdc563438693e00ca3df91a1c2ef03a2a5
Yuan Zi <zy05160516@126.com>
2021-09-13 12:35:51 +0800

    add some code

---
+++ core/src/main/java/top/iznauy/Core.java
@@ -0,0 +1,14 @@
+package top.iznauy;
+
+public class Core {
+
+    public static String description() {
+        return "This is core module.";
+    }
+
+    public static String version() {
+        return "v1.0";
+    }
+
+
+}
---
+++ utils/src/main/java/top/iznauy/Utils.java
@@ -0,0 +1,13 @@
+package top.iznauy;
+
+public class Utils {
+
+    public static String description() {
+        return "This is utils module.";
+    }
+
+    public static String version() {
+        return Core.version();
+    }
+
+}
